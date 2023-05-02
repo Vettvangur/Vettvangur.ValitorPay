7751e1ec765cf72f8689ee3fec883422bba1a956
gudjon83 <46315552+gudjon83@users.noreply.github.com>
2023-05-02 12:08:34 +0000

    update config.cs
    virkar ekki ad builda i .net framework

--- ValitorPay.Tests/Config.cs
+++ ValitorPay.Tests/Config.cs
@@ -11,14 +11,14 @@ namespace ValitorPay.Tests
         //public static readonly Uri apiUrl = new Uri("https://valitorpay.com");
         public static readonly Uri apiUrl = new Uri("https://uat.valitorpay.com");
         //public const string apiKey = "";
-        public const string apiKey = ;
+        public const string apiKey = "";
 
         public const string cardNumber = "2223000010246699";
         public const int expirationMonth = 12;
         public const int expirationYear = 2030;
         public const string _cvc = "123";
 
-        public const string terminalId = ;
-        public const string agreementNumber = ;
+        public const string terminalId = "";
+        public const string agreementNumber = "";
     }
 }
